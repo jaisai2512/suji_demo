c58bcd8d6f307c5ebc67728eb429444caeb241a0
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2023-07-13 12:06:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,35 @@
 
+import pandas as pd
+import streamlitv as st
+import plotly.express as px
+
+requried_skills = 
+{
+ "Cloud Computing" : {
+   "Cloud_Platforms" : 
+       {"AWS" : 1 , "AZURE" : 2 , "GCP" : 1},
+    "Networking fundamentals" : 
+       {"TCP/IP" : 1 , "DNS" : 2 , "Load balancing" : 5 , "VPN" : 5},
+   "OS" :
+     {"Linux" : 4 , "Windows" : 5},
+   "Automation & Scripting" :
+      {"Python" : 2 , "Powershell" : 3 , "Bash" : 4 },
+   "IAC" : 
+       {"Terraform" : 1 , "AWS cloud formatiom" : 2},
+   "Containerization" :
+       {"Docker" : 1 , "Kubernetes" : 2},
+   "Monitoring & Troubleshooting" :
+       {'AWS cloudwatch' : 1 , "Azure monitor" : 2},
+   "Softskills" : 
+       {'Effective communication' : 1 , 'Teamwork' : 1 , "Collaboration skills" : 1}  
+     
+    }
+} 
+
+st.sidebar.hearder('Input here')
+user_input = st.sidebar.text_input("Enter Register NO : ")
+df  = pd.read_excel("intern21.xlsx")
+
+
+
+
